84bd743d49c81920b51aadb2e6f561e50ecc02aa
Anuj verma <68897803+Anujverma89@users.noreply.github.com>
2025-01-11 11:57:10 +0545

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-     origin: 'https://aahar-i3ph.vercel.app/',
+     origin: "*",
   };
   app.use(cors(corsOptions));
   await app.listen(process.env.PORT ?? 5000);
